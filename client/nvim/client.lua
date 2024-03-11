b02116fc23b0350025b170c457d6b5e6a19b5472
hubcio2115 <kowalski.hubercik1@gmail.com>
2024-03-11 18:14:41 +0100

    updated client file for nvim

--- client/nvim/client.lua
+++ client/nvim/client.lua
@@ -1,21 +1,33 @@
-local root_files = { "index.html" }
+local filetypes = { "index.html", "templ" }
 local htmx_path = "/Users/hubertkowalski/Documents/Projects/htmx-ls/target/release/htmx-ls"
 
 vim.lsp.set_log_level("debug")
 
 local function setup()
-  return {
-    default_config = {
-      name = "htmx-ls",
-      cmd = { htmx_path },
-      root_dir = vim.fs.dirname(vim.fs.find(root_files, { upward = true })[1]),
-    },
-  }
+	return {
+		default_config = {
+			cmd = { htmx_path },
+			filetypes = filetypes,
+			single_file_support = true,
+			root_dir = vim.fs.dirname(vim.fs.find({ "index.html" }, { upward = true })[1]),
+		},
+		docs = {
+			description = [[
+https://github.com/hubcio2115/htmx-ls
+
+`htmx-lsp` can be installed via `cargo`:
+```sh
+cargo install htmx-ls
+```
+
+Lsp is still very much work in progress and experimental. Use at your own risk.
+]],
+		},
+	}
 end
 
 vim.lsp.start(setup().default_config)
 
 vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(args)
-  end,
+	callback = function(_) end,
 })
